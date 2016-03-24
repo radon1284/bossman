c43229ddb29aee1d35f29c160828a1b2a79ffd4b
Prelang Builder <builder@prelang.com>
2016-03-24 14:53:51 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Comment' to allow for create, update

--- app/admin/comment.rb
+++ app/admin/comment.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Comment do
+  permit_params :to_do_omments
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
